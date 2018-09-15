2589f3440d68e47d5d4ccf024da4e02802382841
thefattony <anton.alexeyev@outlook.com>
2018-09-15 15:44:16 +0300

    theme integration

---
+++ malevich-web/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+
+
+import {MainPageComponent} from "./main/main-page/main-page.component";
+
+const routes: Routes = [
+  {path: '', redirectTo: '/main-page', pathMatch: 'full'},
+  {path: 'main-page', component: MainPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}

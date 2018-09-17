061ec85eaea1cc2407e85c582d2785c0f45db1b6
thefattony <anton.alexeyev@outlook.com>
2018-09-17 15:52:32 +0300

    main menu fixes

--- malevich-web/src/app/app.component.ts
+++ malevich-web/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {AfterViewInit, Component} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import {Globals} from "./globals";
+import {LoginService} from "./_services";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,28 @@ import {AfterViewInit, Component} from '@angular/core';
 export class AppComponent implements AfterViewInit{
   title = 'malevich-web';
 
+  constructor(public translate: TranslateService, public globals: Globals, private loginService: LoginService) {
+    translate.addLangs(['en', 'ru']);
+    translate.setDefaultLang('en');
+
+    const browserLang: string = translate.getBrowserLang();
+    const lang: string = localStorage.getItem('currentLanguage');
+
+    if (lang === null) {
+      translate.use(browserLang.match(/en|ru/) ? browserLang : 'en');
+    } else {
+      translate.use(lang);
+    }
+
+
+    if (localStorage.getItem('currentUser')) {
+      this.globals.isAuthorised = true;
+      loginService.refreshToken();
+      if (!localStorage.getItem('user')) {
+        loginService.logout();
+      }
+    }
+  }
   ngAfterViewInit(): void {
     $['HSCore'].components.HSGoTo.init('.js-go-to');
   }

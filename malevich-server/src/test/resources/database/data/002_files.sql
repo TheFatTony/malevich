b334060693be7a4063ad7bcde7ca0d5bb1450bff
thefattony <anton.alexeyev@outlook.com>
2018-09-19 13:15:11 +0300

    database artworks migration

--- malevich-server/src/test/resources/database/data/002_files.sql
+++ malevich-server/src/test/resources/database/data/002_files.sql
@@ -1,6 +1,2 @@
 INSERT INTO files (id, file_name, file_size, mime_type, url, alt)
-VALUES (1, 'view-lot-1.jpg', NULL, 'image', NULL, NULL),
-       (2, 'view-lot-2.jpg', NULL, 'image', NULL, NULL),
-       (3, 'view-lot-2.jpg', NULL, 'image', NULL, NULL),
-       (4, 'art-work.jpg', NULL, 'image', NULL, NULL),
-       (5, 'art-work1.jpg', NULL, 'image', NULL, NULL);
+VALUES (1, 'img-480-700.jpg', NULL, 'image', NULL, NULL);

56e79edd8a850f2bb043a7b548e94ea9e468d84f
thefattony <anton.alexeyev@outlook.com>
2018-09-25 19:25:59 +0300

    user profile prototype

--- malevich-server/src/main/java/io/malevich/server/entity/enums/Role.java
+++ malevich-server/src/main/java/io/malevich/server/entity/enums/Role.java
@@ -3,7 +3,7 @@ package io.malevich.server.entity.enums;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+    USER("ROLE_USER"), ADMIN("ROLE_ADMIN"), GALLERY("ROLE_GALLERY"), TRADER("ROLE_TRADER");
 
     private String authority;
 

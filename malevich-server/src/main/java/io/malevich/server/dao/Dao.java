a1d151afe6126d1d2f4fb859d469ba61a6e8bbc1
thefattony <anton.alexeyev@outlook.com>
2018-09-26 22:15:46 +0300

    save work

--- malevich-server/src/main/java/io/malevich/server/dao/Dao.java
+++ malevich-server/src/main/java/io/malevich/server/dao/Dao.java
@@ -2,11 +2,16 @@ package io.malevich.server.dao;
 
 
 import io.malevich.server.entity.Entity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface Dao<T extends Entity, I> {
+    
     List<T> findAll();
+
+    Page<T> findAll(Pageable pageable);
 
     T find(I id);
 
--- malevich-server/src/main/java/io/malevich/server/dao/JpaDao.java
+++ malevich-server/src/main/java/io/malevich/server/dao/JpaDao.java
@@ -1,17 +1,22 @@
 package io.malevich.server.dao;
 
 import io.malevich.server.entity.Entity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaDao<T extends Entity, I> implements Dao<T, I> {
 
+    protected Class<T> entityClass;
     @PersistenceContext
     EntityManager entityManager;
-
-    protected Class<T> entityClass;
 
     public JpaDao(Class<T> entityClass) {
         this.entityClass = entityClass;
@@ -27,8 +32,22 @@ public class JpaDao<T extends Entity, I> implements Dao<T, I> {
     }
 
     public List<T> findAll() {
-        return entityManager.createQuery("from " + entityClass.getName())
-                .getResultList();
+        final CriteriaBuilder builder = this.getEntityManager().getCriteriaBuilder();
+        final CriteriaQuery<T> criteriaQuery = builder.createQuery(this.entityClass);
+
+        Root<T> root = criteriaQuery.from(this.entityClass);
+
+        criteriaQuery.select(root);
+        TypedQuery<T> typedQuery = this.getEntityManager().createQuery(criteriaQuery);
+        return typedQuery.getResultList();
+    }
+
+    @Override
+    public Page<T> findAll(Pageable pageable) {
+        if (pageable.isUnpaged())
+        return null;
+
+
     }
 
     public void create(T entity) {

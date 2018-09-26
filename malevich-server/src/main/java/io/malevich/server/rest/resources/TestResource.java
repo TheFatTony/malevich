b5f100da96d15c5defaac5c1897422d3d13fd6a9
thefattony <anton.alexeyev@outlook.com>
2018-09-27 00:37:29 +0300

    transactions verified

--- malevich-server/src/main/java/io/malevich/server/rest/resources/TestResource.java
+++ malevich-server/src/main/java/io/malevich/server/rest/resources/TestResource.java
@@ -11,6 +11,8 @@ import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,22 +43,19 @@ public class TestResource {
     private MessageSource messageSource;
 
     @RequestMapping(method = RequestMethod.GET)
+    @Transactional
     public String test() {
         AccessTokenEntity accessTokenEntity = new AccessTokenEntity(userDao.findByName("admin@malevich.io"), "fhgjkl");
         accessTokenDao.save(accessTokenEntity);
 
+        VelocityContext context = new VelocityContext();
+        context.put("test", "World !!!");
 
-
-        throw new NullPointerException();
-
-//        VelocityContext context = new VelocityContext();
-//        context.put("test", "World !!!");
-//
-//        StringWriter stringWriter = new StringWriter();
-//        stringWriter.append(messageSource.getMessage("welcome.message", null, LocaleContextHolder.getLocale()));
+        StringWriter stringWriter = new StringWriter();
+        stringWriter.append(messageSource.getMessage("welcome.message", null, LocaleContextHolder.getLocale()));
 //        velocityEngine.mergeTemplate("templates/mail/confirm_email.vm", "UTF-8", context, stringWriter);
 //        mailQueueService.create(new MailQueue("anton.alexeyev85@gmail.com", "Testing Subject", stringWriter.toString()));
-//        return stringWriter.toString();
+        return stringWriter.toString();
     }
 
 

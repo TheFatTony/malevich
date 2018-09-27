265c0f1cebad0545846a06df33c8b0b6095e15a9
thefattony <anton.alexeyev@outlook.com>
2018-09-27 10:16:48 +0300

    Caching

--- malevich-server/src/main/java/io/malevich/server/dao/artwork/ArtworkDao.java
+++ malevich-server/src/main/java/io/malevich/server/dao/artwork/ArtworkDao.java
@@ -2,11 +2,14 @@ package io.malevich.server.dao.artwork;
 
 import io.malevich.server.entity.ArtworkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ArtworkDao extends JpaRepository<ArtworkEntity, Long>, CustomArtworkDao {
+import java.util.List;
 
-//    @Query("select ae from ArtworkEntity ae join fetch ae.category left join fetch ae.thumbnail left join fetch ae.image")
-//    List<ArtworkEntity> findAll();
+@Repository
+public interface ArtworkDao extends JpaRepository<ArtworkEntity, Long> {
+
+    @Query("select ae from ArtworkEntity ae join fetch ae.category left join fetch ae.thumbnail left join fetch ae.image")
+    List<ArtworkEntity> findAll();
 }
--- malevich-server/src/main/java/io/malevich/server/dao/artwork/CustomArtworkDao.java
+++
@@ -1,9 +0,0 @@
-package io.malevich.server.dao.artwork;
-
-import io.malevich.server.entity.ArtworkEntity;
-
-import java.util.List;
-
-public interface CustomArtworkDao {
-    List<ArtworkEntity> findAll();
-}

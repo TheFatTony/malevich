5b56cb3d446a80a9e200091f298619f7463ff15c
thefattony <anton.alexeyev@outlook.com>
2018-10-02 12:33:41 +0300

    prototype implemented

--- malevich-web/src/app/auth/login/login.component.ts
+++ malevich-web/src/app/auth/login/login.component.ts
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
 
     this.authService.logout();
 
-    this.returnUrl = '..'+ this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
   get f() {

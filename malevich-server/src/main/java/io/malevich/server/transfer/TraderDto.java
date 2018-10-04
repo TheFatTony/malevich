21699b5f5dd95e20fdbdca42e40027a28d56d1ad
toolstik <viktor.tolstik@malevich.io>
2018-10-04 13:15:52 +0300

    trader profile translation

--- malevich-server/src/main/java/io/malevich/server/transfer/TraderDto.java
+++ malevich-server/src/main/java/io/malevich/server/transfer/TraderDto.java
@@ -1,5 +1,6 @@
 package io.malevich.server.transfer;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,7 @@ public class TraderDto {
 
     private String mobile;
 
+    @JsonFormat(pattern = "MM/dd/yyyy")
     private Date dateOfBirth;
 
     private char gender;

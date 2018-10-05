7cf1028d7c4d2aebfbbc1554bf84a4846192c3da
thefattony <anton.alexeyev@outlook.com>
2018-10-05 15:17:25 +0300

    apache fop

---
+++ shell/restartDev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export LC_ALL=en_US.UTF-8
+ssh -i "~/Documents/amazon/nvirginiadev.pem" ec2-user@ec2-34-238-136-233.compute-1.amazonaws.com "sudo docker restart mysql"
+ssh -i "~/Documents/amazon/nvirginiadev.pem" ec2-user@ec2-34-238-136-233.compute-1.amazonaws.com "sudo docker restart tomcat"

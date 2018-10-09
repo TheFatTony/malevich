5a85d25137b26ded222ddf107d2130cc984e4cf3
toolstik <viktor.tolstik@malevich.io>
2018-10-09 16:09:06 +0300

    server side

---
+++ malevich-server/src/main/java/io/malevich/server/services/address/country/AddressServiceImpl.java
@@ -0,0 +1,38 @@
+package io.malevich.server.services.address.country;
+
+
+import io.malevich.server.dao.address.AddressDao;
+import io.malevich.server.entity.AddressEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+
+    @Autowired
+    private AddressDao addressDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<AddressEntity> findAll() {
+        return this.addressDao.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public AddressEntity find(Long id) {
+        return this.addressDao.findById(id).get();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<AddressEntity> findByTraderId(Long traderId) {
+        return this.addressDao.findByTreaderId(traderId);
+    }
+
+}

e44edebfa9d328af71b94f418a5aae459e4b4df9
thefattony <anton.alexeyev@outlook.com>
2018-10-12 18:38:06 +0300

    transactions enities

---
+++ malevich-server/src/main/java/io/malevich/server/entity/TransactionsEntity.java
@@ -0,0 +1,54 @@
+package io.malevich.server.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+
+@javax.persistence.Entity
+@Table(name = "transactions")
+public class TransactionsEntity implements Entity {
+
+
+  @Getter
+  @Setter
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Getter
+  @Setter
+  @Column(name = "type_id")
+  private String typeId;
+
+  @Getter
+  @Setter
+  @Column(name = "effective_date")
+  private java.sql.Timestamp effectiveDate;
+
+  @Getter
+  @Setter
+  @Column(name = "party_id")
+  private long partyId;
+
+  @Getter
+  @Setter
+  @Column(name = "counterparty_id")
+  private long counterpartyId;
+
+  @Getter
+  @Setter
+  @Column(name = "artwork_id")
+  private long artworkId;
+
+  @Getter
+  @Setter
+  @Column(name = "amount")
+  private double amount;
+
+  @Getter
+  @Setter
+  @Column(name = "quantity")
+  private long quantity;
+
+}

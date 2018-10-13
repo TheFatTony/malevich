bb4aa69f05ae46ef82a14c67ec7eb84c1e340bbf
thefattony <anton.alexeyev@outlook.com>
2018-10-13 19:34:07 +0300

    gallery profile prototype

---
+++ malevich-web/src/app/profile/gallery-profile/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-gallery-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

081d25eded5f718774b65403cde1e66d90ee7136
thefattony <anton.alexeyev@outlook.com>
2018-10-22 23:17:31 +0300

    artworks view

--- malevich-web/src/app/profile/gallery-profile/artwork-stock/artwork-stock.component.ts
+++ malevich-web/src/app/profile/gallery-profile/artwork-stock/artwork-stock.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ArtworkStockService} from "../../../_services/artwork-stock.service";
+import {TranslateService} from "@ngx-translate/core";
+import {ArtworkStockDto} from "../../../_transfer/artworkStockDto";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-profile-gallery-artwork-stock',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtworkStockComponent implements OnInit {
 
-  constructor() { }
+  artworkStocks: ArtworkStockDto[];
+
+  public url = environment.baseUrl;
+
+  constructor(private artworkStockService: ArtworkStockService, public translate: TranslateService) { }
 
   ngOnInit() {
+    this.getArtworkStock();
+  }
+
+  getArtworkStock(): void {
+    this.artworkStockService
+      .getArtworkStock()
+      .subscribe(
+        data => (this.artworkStocks = data)
+      );
   }
 
 }
--- malevich-web/src/app/profile/gallery-profile/orders/orders.component.ts
+++ malevich-web/src/app/profile/gallery-profile/orders/orders.component.ts
@@ -3,6 +3,7 @@ import {OrderDto} from "../../../_transfer/orderDto";
 import {OrderService} from "../../../_services/order.service";
 import {TranslateService} from "@ngx-translate/core";
 import {jqxGridComponent} from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxgrid';
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-profile-gallery-orders',
@@ -13,47 +14,13 @@ export class OrdersComponent implements OnInit, AfterViewInit {
 
   orders: OrderDto[];
 
-  @ViewChild('myGrid') myGrid: jqxGridComponent;
+  public url = environment.baseUrl;
 
-  public columns =
-    [
-      { text: 'Date', datafield: 'Date', columntype: 'textbox', width: '20%' },
-      { text: 'Amount', datafield: 'Amount',columntype: 'textbox', width: '20%' },
-      { text: 'Artwork', datafield: 'Artwork',columntype: 'textbox', width: '20%' },
-      { text: 'Trade Type', datafield: 'Trade Type',columntype: 'textbox', width: '20%' },
-      { text: 'Type', datafield: 'Type',columntype: 'textbox', width: '20%' }
-    ];
-
-  constructor(private orderService: OrderService, public translateService: TranslateService) {
+  constructor(public orderService: OrderService, public translate: TranslateService) {
   }
 
   ngAfterViewInit(): void {
-    this.createButtons();
   }
-
-  createButtonsContainers(statusbar: any): void {
-    let buttonsContainer = document.createElement('div');
-    buttonsContainer.style.cssText = 'overflow: hidden; position: relative; margin: 5px; height: 60px;';
-    let addButtonContainer = document.createElement('div');
-    addButtonContainer.id = 'addButton';
-    addButtonContainer.style.cssText = 'float: left; margin-left: 5px;';
-    buttonsContainer.appendChild(addButtonContainer);
-    statusbar[0].appendChild(buttonsContainer);
-  }
-
-  createButtons(): void {
-    let addButtonOptions = {
-      width: 100, height: 25, value: 'Place an ask',
-      imgSrc: 'https://www.jqwidgets.com/angular/images/add.png',
-      imgPosition: 'center', textPosition: 'center',
-      textImageRelation: 'imageBeforeText'
-    }
-    let addButton = jqwidgets.createInstance('#addButton', 'jqxButton', addButtonOptions);
-    addButton.addEventHandler('click', (event: any): void => {
-    });
-  }
-
-
 
   ngOnInit() {
     this.getPlacedOrders();

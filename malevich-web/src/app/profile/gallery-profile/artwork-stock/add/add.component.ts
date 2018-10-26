c4d563aee04fb077dcc4dd3a41108cdfe060ad73
toolstik <tolstik.va@gmail.com>
2018-10-26 18:44:28 +0300

    fuckin comboboxes selection

--- malevich-web/src/app/profile/gallery-profile/artwork-stock/add/add.component.ts
+++ malevich-web/src/app/profile/gallery-profile/artwork-stock/add/add.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {ArtworkDto, GalleryDto} from "../../../../_transfer";
+import {ArtistDto, ArtworkDto, CategoryDto, GalleryDto} from "../../../../_transfer";
 import {ArtworkStockDto} from "../../../../_transfer/artworkStockDto";
 import {GalleryService} from "../../../../_services/gallery.service";
 import {ArtworkStockService} from "../../../../_services/artwork-stock.service";
@@ -7,6 +7,8 @@ import {ArtworkService} from "../../../../_services/artwork.service";
 import {TranslateService} from "@ngx-translate/core";
 import {jqxComboBoxComponent} from "jqwidgets-scripts/jqwidgets-ts/angular_jqxcombobox";
 import {Router} from "@angular/router";
+import {ArtistService} from "../../../../_services/artist.service";
+import {CategoryService} from "../../../../_services/category.service";
 
 @Component({
   selector: 'app-profile-gallery-artwork-stock-add',
@@ -16,27 +18,33 @@ import {Router} from "@angular/router";
 export class AddComponent implements OnInit {
 
   @ViewChild('addArtWorkComboBox') addArtWorkComboBox: jqxComboBoxComponent;
+  @ViewChild('artistComboBox') artistComboBox: jqxComboBoxComponent;
+  @ViewChild('categoryComboBox') categoryComboBox: jqxComboBoxComponent;
 
   gallery: GalleryDto;
-  artworks: ArtworkDto[];
 
-  public addArtwork: ArtworkDto;
-  public selectedArtwork: ArtworkDto;
-  public artworkStock: ArtworkStockDto;
+  public artwork: ArtworkDto;
+
   public addArtWorkComboBoxSource: any[];
+  artists: any[];
+  categories: any[];
 
   constructor(private router: Router,
               private galleryService: GalleryService,
               private artworkStockService: ArtworkStockService,
               private artworkService: ArtworkService,
+              private artistService: ArtistService,
+              private categoryService: CategoryService,
               public translate: TranslateService) {
     $.jqx.theme = 'malevich';
   }
 
   ngOnInit() {
     this.getArtworks();
-    //this.addArtwork = this.getDefaultArtWork();
-    // this.addArtwork.titleMl = new Map<string, string>();
+    //this.artwork = this.getDefaultArtWork();
+    // this.artwork.titleMl = new Map<string, string>();
+    this.getArtists();
+    this.getCategories();
   }
 
   getDefaultArtWork() {
@@ -51,7 +59,6 @@ export class AddComponent implements OnInit {
       .getArtworks()
       .subscribe(
         data => {
-          this.artworks = data;
           this.addArtWorkComboBoxSource = data.map(artwork => ({
               id: artwork,
               title: artwork.titleMl[this.translate.currentLang],
@@ -61,29 +68,109 @@ export class AddComponent implements OnInit {
                 '<span class="d-block g-color-lightred">' + artwork.category.categoryNameMl[this.translate.currentLang] + '</span>' + '</td></tr></table>'
             })
           );
-          // this.addArtWorkComboBoxSource.unshift(this.getDefaultArtWork());
+
+          // console.info(this.addArtWorkComboBoxSource);
         }
       );
   }
 
+  getArtists() {
+    this.artistService
+      .getArtists()
+      .subscribe(data => (
+        this.artists = data.map(i => ({
+          title: i.fullNameMl[this.translate.currentLang],
+          value: i
+        }))
+      ));
+  }
+
+  getCategories() {
+    this.categoryService
+      .getCategories()
+      .subscribe(data => (
+        this.categories = data.map(i => ({
+          title: i.categoryNameMl[this.translate.currentLang],
+          value: i
+        }))
+      ));
+  }
+
   submit() {
     let addArtworkStock = new ArtworkStockDto();
-    addArtworkStock.artwork = this.artworkStock.artwork;
+    addArtworkStock.artwork = this.artwork;
     addArtworkStock.gallery = this.gallery;
     this.artworkStockService.addArtworkStock(addArtworkStock).subscribe();
     this.router.navigate(['/profile/gallery/artworkstok']);
   }
 
   onArtworkComboBoxChange($event) {
-    this.selectedArtwork = $event;
-    if (this.selectedArtwork) {
-      this.artworkStock = new ArtworkStockDto();
-      this.artworkStock.artwork = this.selectedArtwork;
-      this.addArtWorkComboBox.disabled(true);
-    }
+    // console.info($event);
+
+    if (!$event)
+      return;
+
+    this.artwork = $event;
+
+    let artistIndex = this.artists.findIndex(value => value.value.id == this.artwork.artist.id);
+    this.artistComboBox.selectIndex(artistIndex);
+
+    let categoryIndex = this.categories.findIndex(value => value.value.id == this.artwork.category.id);
+    this.categoryComboBox.selectIndex(categoryIndex);
+
+    // console.info(this.artwork);
+    // this.addArtWorkComboBox.disabled(true);
   }
 
   cancel() {
     this.router.navigate(['/profile/gallery/artworkstok']);
+  }
+
+  onArtistComboBoxChange($event) {
+    // console.info($event);
+
+    if (!$event)
+      return;
+
+    if (!this.artwork)
+      this.artwork = new ArtworkDto();
+
+    this.artwork.artist = $event;
+  }
+
+  onCategoryComboBoxChange($event) {
+    if (!$event)
+      return;
+
+    if (!this.artwork)
+      this.artwork = new ArtworkDto();
+
+    this.artwork.category = $event;
+  }
+  
+  onDescriptionEditorChange($event) {
+    if (!$event)
+      return;
+
+    if (!this.artwork)
+      this.artwork = new ArtworkDto();
+
+    if (!this.artwork.descriptionMl)
+      this.artwork.descriptionMl = new Map<string, string>();
+
+    this.artwork.descriptionMl['en'] = $event
+  }
+
+  onTitleEditorChange($event) {
+    if (!$event)
+      return;
+
+    if (!this.artwork)
+      this.artwork = new ArtworkDto();
+
+    if (!this.artwork.titleMl)
+      this.artwork.titleMl = new Map<string, string>();
+
+    this.artwork.titleMl['en'] = $event
   }
 }

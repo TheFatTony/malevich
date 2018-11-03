e1807a96534816a4c58ee5fe3466712ea32f5ac7
thefattony <anton.alexeyev@outlook.com>
2018-11-03 17:18:24 +0300

    lb refactoring

--- malevich-web/src/environments/environment.prod.ts
+++ malevich-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://ec2-34-238-136-233.compute-1.amazonaws.com:8080/malevich-srv/'
+  baseUrl: 'http://malevich-test.j.layershift.co.uk:8080/'
 };

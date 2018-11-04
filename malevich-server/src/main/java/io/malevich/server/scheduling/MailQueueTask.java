f61d232b236c0ab0fa3b2a5f12007e368b01cc55
thefattony <anton.alexeyev@outlook.com>
2018-11-04 11:49:33 +0300

    Send mail moved to test only mode

--- malevich-server/src/main/java/io/malevich/server/scheduling/MailQueueTask.java
+++ malevich-server/src/main/java/io/malevich/server/scheduling/MailQueueTask.java
@@ -2,10 +2,12 @@ package io.malevich.server.scheduling;
 
 import io.malevich.server.services.mailqueue.MailQueueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
+@Profile("test")
 @Component
 public class MailQueueTask {
 

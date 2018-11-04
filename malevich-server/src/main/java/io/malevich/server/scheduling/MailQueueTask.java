9bb29a854116a77675cee571d398a3e95e6ea7bc
thefattony <anton.alexeyev@outlook.com>
2018-11-04 10:46:35 +0300

    connection keep alive

--- malevich-server/src/main/java/io/malevich/server/scheduling/MailQueueTask.java
+++ malevich-server/src/main/java/io/malevich/server/scheduling/MailQueueTask.java
@@ -2,6 +2,7 @@ package io.malevich.server.scheduling;
 
 import io.malevich.server.services.mailqueue.MailQueueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
@@ -12,7 +13,7 @@ public class MailQueueTask {
     private MailQueueService mailQueueService;
 
 
-    //    @Scheduled(initialDelay = 2000, fixedRate = 10000)
+    @Scheduled(initialDelay = 2000, fixedRate = 10000)
     public void reportCurrentTime() {
         mailQueueService.sendAllMail();
     }

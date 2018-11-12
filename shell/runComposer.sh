a264b28d08438fea2fc3725ae1087b6fd38c32eb
thefattony <anton.alexeyev@outlook.com>
2018-11-12 19:06:54 +0300

    minor changes

--- shell/runComposer.sh
+++ shell/runComposer.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 export LC_ALL=en_US.UTF-8
+export FABRIC_VERSION=hlfv11
 ssh -i "~/Documents/amazon/nvirginiadev.pem" ec2-user@ec2-34-238-136-233.compute-1.amazonaws.com "setsid composer-playground &> /dev/null"

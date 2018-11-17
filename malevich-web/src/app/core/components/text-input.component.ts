cf4d968844297ea86b3a75ad5f59c490ed4b803d
toolstik <tolstik.va@gmail.com>
2018-11-17 09:32:35 +0300

    merge fixes

--- malevich-web/src/app/core/components/text-input.component.ts
+++ malevich-web/src/app/core/components/text-input.component.ts
@@ -4,7 +4,7 @@ import {jqxInputComponent} from "jqwidgets-scripts/jqwidgets-ts/angular_jqxinput
 
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => InputComponent),
+  useExisting: forwardRef(() => TextInputComponent),
   multi: true
 };
 
@@ -13,7 +13,7 @@ export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
   template: '<input class="g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v3 rounded g-py-15 g-px-15" type="text" [(ngModel)]="ngValue">',
   providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class InputComponent extends jqxInputComponent {
+export class TextInputComponent extends jqxInputComponent {
 
   constructor(containerElement: ElementRef) {
     super(containerElement);

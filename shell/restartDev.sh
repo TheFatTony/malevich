cd6860a496b5952e93db6b6e1d98c441f753a21c
thefattony <anton.alexeyev@outlook.com>
2018-11-20 09:23:59 +0300

    new server related urls

--- shell/restartDev.sh
+++ shell/restartDev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export LC_ALL=en_US.UTF-8
-ssh -i "~/Documents/amazon/nvirginiadev.pem" ec2-user@ec2-34-238-136-233.compute-1.amazonaws.com "sudo docker restart mysql"
-ssh -i "~/Documents/amazon/nvirginiadev.pem" ec2-user@ec2-34-238-136-233.compute-1.amazonaws.com "sudo docker restart tomcat"
+ssh -i "~/Documents/amazon/blckkey.pem" ubuntu@ec2-35-153-69-63.compute-1.amazonaws.com "sudo docker restart mysql"
+ssh -i "~/Documents/amazon/blckkey.pem" ubuntu@ec2-35-153-69-63.compute-1.amazonaws.com "sudo docker restart tomcat"

c3290a01fa6c6d17833546975beedd01848986fc
thefattony <anton.alexeyev@outlook.com>
2018-11-22 12:22:10 +0300

    quickfix

--- malevich-web/src/app/profile/trader-profile/edit/edit.component.ts
+++ malevich-web/src/app/profile/trader-profile/edit/edit.component.ts
@@ -70,9 +70,9 @@ export class EditComponent implements OnInit, AfterViewInit {
 
   onUploadEnd(event: any): void {
     let args = event.args;
-    let serverResponse = args.response.toString()
+    let serverResponse = JSON.parse(args.response.toString()
       .replace('<pre style="word-wrap: break-word; white-space: pre-wrap;">', '')
-      .replace('</pre>', '');
+      .replace('</pre>', ''));
     this.trader.thumbnail = serverResponse;
     console.log(this.trader);
   }

0ba25abc067b2f475de253b33de9e13cb0ea5d1d
toolstik <tolstik.va@gmail.com>
2018-11-28 13:03:54 +0300

    some new validation directives

---
+++ malevich-web/src/app/_directives/validators/mobile/mobile.directive.ts
@@ -0,0 +1,24 @@
+import {Attribute, ContentChild, Directive, forwardRef} from '@angular/core';
+import {AbstractControl, NG_VALIDATORS, NgModel, ValidationErrors, Validator, Validators} from "@angular/forms";
+
+@Directive({
+  selector: '[mchMobile][formControlName],[mchMobile][formControl],[mchMobile][ngModel]',
+  providers: [
+    {provide: NG_VALIDATORS, useExisting: forwardRef(() => MobileDirective), multi: true}
+  ]
+})
+export class MobileDirective implements Validator {
+
+  @ContentChild(NgModel) ngModel: NgModel;
+
+  private patternValidator = Validators.pattern(/\+\d{1,3} \(\d{3}\) \d{3}-\d{4}/);
+
+  validate(control: AbstractControl): ValidationErrors | null {
+
+    if (this.patternValidator(control) == null)
+      return null;
+
+    return {mchMobile: `Value is not a valid phone number`};
+  }
+
+}

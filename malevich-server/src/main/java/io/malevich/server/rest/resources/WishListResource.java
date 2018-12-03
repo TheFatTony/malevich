5400809be1b368c7398b8ec17deb672cd6bb762f
Harshil Modi <harshilm@elegantmicroweb.com>
2018-12-03 19:13:20 +0530

    work on wish list with pagination

---
+++ malevich-server/src/main/java/io/malevich/server/rest/resources/WishListResource.java
@@ -0,0 +1,60 @@
+package io.malevich.server.rest.resources;
+
+import io.malevich.server.domain.WishListEntity;
+import io.malevich.server.services.wishlist.WishListService;
+import io.malevich.server.transfer.PageResponseDto;
+import io.malevich.server.transfer.PageSortableRequestDto;
+import io.malevich.server.transfer.WishListDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/wishlist")
+public class WishListResource {
+
+    @Autowired
+    private WishListService wishListService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PreAuthorize("hasRole('TRADER')")
+    @PostMapping("/addWish")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Void> save(@RequestBody WishListDto dto) {
+        this.wishListService.save(convertToEntity(dto));
+        return ResponseEntity.ok().build();
+    }
+
+    //@PreAuthorize("hasRole('TRADER')")
+    @PostMapping("/list")
+    @ResponseStatus(HttpStatus.OK)
+    public PageResponseDto list(@RequestBody PageSortableRequestDto requestDto) {
+        Page<WishListEntity> resultPage = this.wishListService.findAll(PageRequest.of(requestDto.getPage(), requestDto.getSize()));
+        return new PageResponseDto(resultPage.getContent().stream().map(pageEntry -> convertToDto(pageEntry)).collect(Collectors.toList()), resultPage.getTotalElements(), resultPage.getTotalPages());
+    }
+
+    @DeleteMapping("/remove/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        this.wishListService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    private WishListEntity convertToEntity(WishListDto dto) {
+        return modelMapper.map(dto, WishListEntity.class);
+    }
+
+    private WishListDto convertToDto(WishListEntity entity) {
+        return modelMapper.map(entity, WishListDto.class);
+    }
+
+}
--- malevich-web/src/app/profile/trader-profile/wishlist/wishlist.component.ts
+++ malevich-web/src/app/profile/trader-profile/wishlist/wishlist.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {WishListService} from '../../../_services/wish-list.service';
+import {TranslateService} from '@ngx-translate/core';
+import {PageSortableRequestDto} from '../../../_transfer/pageSortableRequestDto';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile-trader-wishlist',
@@ -7,10 +11,37 @@ import {Component, OnInit} from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
-  constructor() {
+  wishList: any = {};
+  pageSortable: PageSortableRequestDto;
+
+  constructor(private wishListService: WishListService, private translate: TranslateService, private router: Router) {
   }
 
   ngOnInit() {
+    this.pageSortable = new PageSortableRequestDto();
+    this.pageSortable.page = 0;
+    this.pageSortable.size = 3;
+    this.getWishList(this.pageSortable);
+  }
+
+  setPage(page: number) {
+    this.pageSortable.page = page;
+    this.getWishList(this.pageSortable);
+  }
+
+  getWishList(pageObj: any) {
+    return this.wishListService.getWishList(pageObj).subscribe(data => {
+      this.wishList = data.body;
+      this.wishList.currentPage = pageObj.page + 1;
+      this.wishList.size = pageObj.size;
+    });
+  }
+
+  removeWish(id: number, currentPage: number) {
+    return this.wishListService.removeWish(id).subscribe(data => {
+      this.pageSortable.page = currentPage;
+      this.getWishList(this.pageSortable);
+    });
   }
 
 }

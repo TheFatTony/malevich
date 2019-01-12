88327969c8904e1fa7456c90958c43f7b1e93c8f
thefattony <anton.alexeyev@outlook.com>
2019-01-12 10:04:12 +0300

    test framework

--- malevich-server/src/test/java/io/malevich/server/AppTest.java
+++ malevich-server/src/test/java/io/malevich/server/AppTest.java
@@ -1,11 +1,10 @@
 package io.malevich.server;
 
+import com.yinyang.core.server.test.BaseAppTest;
+import com.yinyang.core.server.test.TestApplicationContextInitializer;
 import io.malevich.server.domain.PersonEntity;
 import io.malevich.server.repositories.person.PersonDao;
 import io.malevich.server.services.person.PersonService;
-import io.malevich.server.transfer.PersonDto;
-import io.malevich.server.utils.TestApplicationContextInitializer;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
@@ -13,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.testcontainers.containers.MySQLContainer;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -21,13 +19,8 @@ import static junit.framework.TestCase.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MalevichServerApplication.class)
 @ContextConfiguration(initializers = TestApplicationContextInitializer.class)
-public class AppTest {
+public class AppTest extends BaseAppTest {
 
-    @ClassRule
-    public static MySQLContainer mysql = new MySQLContainer("mysql:5.7.22");
-
-//    @ClassRule
-//    public static GenericContainer redis = new GenericContainer("hyperledger/composer-playground").withExposedPorts(8080);
 
     @Autowired
     private ModelMapper modelMapper;

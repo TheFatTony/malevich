95d6178f3bf64dfaa4edd4ad43b5b23f1c8aa959
toolstik <tolstik.va@gmail.com>
2019-01-25 22:06:28 +0300

    different user agreement for each user type

--- malevich-server/src/main/java/io/malevich/server/services/termsandconditions/TermsAndConditionsService.java
+++ malevich-server/src/main/java/io/malevich/server/services/termsandconditions/TermsAndConditionsService.java
@@ -3,8 +3,10 @@ package io.malevich.server.services.termsandconditions;
 import io.malevich.server.transfer.TermsAndConditionsDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public interface TermsAndConditionsService {
-    TermsAndConditionsDto getByLang(String lang);
+    List<TermsAndConditionsDto> getByLang(String lang);
 }

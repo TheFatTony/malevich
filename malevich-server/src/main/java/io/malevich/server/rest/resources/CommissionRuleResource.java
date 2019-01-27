dbd53079b5b74f2e6122915edfda18874b65e7d2
toolstik <tolstik.va@gmail.com>
2019-01-27 23:52:17 +0300

    create/update

--- malevich-server/src/main/java/io/malevich/server/rest/resources/CommissionRuleResource.java
+++ malevich-server/src/main/java/io/malevich/server/rest/resources/CommissionRuleResource.java
@@ -7,6 +7,8 @@ import io.malevich.server.transfer.CommissionRuleDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,9 +31,27 @@ public class CommissionRuleResource extends RestResource<CommissionRuleDto, Comm
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<CommissionRuleDto> list() {
         List<CommissionRuleEntity> allEntries = this.commissionRuleService.findAll();
         return allEntries.stream().map(allEntry -> convertToDto(allEntry)).collect(Collectors.toList());
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Void> save(@RequestBody CommissionRuleDto artist) {
+        this.commissionRuleService.save(convertToEntity(artist));
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = "/item/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public CommissionRuleDto item(@PathVariable("id") long id) {
+        CommissionRuleEntity allEntry = this.commissionRuleService.find(id);
+        return convertToDto(allEntry);
     }
 
 }

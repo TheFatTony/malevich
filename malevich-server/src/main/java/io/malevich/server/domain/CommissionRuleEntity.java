8b115e230d76493aa153e79f6d72206ef974c04e
toolstik <tolstik.va@gmail.com>
2019-01-27 23:55:57 +0300

    entity validation

--- malevich-server/src/main/java/io/malevich/server/domain/CommissionRuleEntity.java
+++ malevich-server/src/main/java/io/malevich/server/domain/CommissionRuleEntity.java
@@ -7,6 +7,9 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 
@@ -18,12 +21,15 @@ public class CommissionRuleEntity extends YAbstractPersistable<Long> {
     @Getter
     @Setter
     @Column(name = "name")
+    @NotNull
+    @NotBlank
     private String name;
 
 
     @Getter
     @Setter
     @Column(name = "value")
+    @Positive
     private BigDecimal value;
 
 }

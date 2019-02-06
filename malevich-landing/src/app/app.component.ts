0092f6462c3d22bcbeeb40d1a9347465df2b1585
thefattony <anton.alexeyev@outlook.com>
2019-02-06 13:24:50 +0300

    template implemeted

--- malevich-landing/src/app/app.component.ts
+++ malevich-landing/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'malevich-landing';
+
+  ngAfterViewInit(): void {
+// initialization of master slider
+
+
+    // initialization of carousel
+    $['HSCore'].components.HSCarousel.init('.js-carousel');
+
+    // initialization of header
+    $['HSCore'].components.HSHeader.init($('#js-header'));
+    $['HSCore'].helpers.HSHamburgers.init('.hamburger');
+
+    // initialization of go to section
+    $ ['HSCore'].components.HSGoTo.init('.js-go-to');
+
+    // initialization of rating
+    $['HSCore'].components.HSRating.init($('.js-rating'), {
+      spacing: 2
+    });
+
+
+  }
+
 }

138bab44fbefecd95907e2bae237339c34549fdd
thefattony <anton.alexeyev@outlook.com>
2019-02-07 15:54:19 +0300

    send funds

--- malevich-server/src/main/java/io/malevich/server/scheduling/BitcoinBalanceCheck.java
+++ malevich-server/src/main/java/io/malevich/server/scheduling/BitcoinBalanceCheck.java
@@ -7,14 +7,12 @@ import io.malevich.server.domain.PaymentMethodEntity;
 import io.malevich.server.domain.enums.ExchangeOrderStatus;
 import io.malevich.server.repositories.paymentmethod.PaymentMethodDao;
 import io.malevich.server.services.exchangeorder.ExchangeOrderService;
-import org.bitcoinj.core.Address;
-import org.bitcoinj.core.BlockChain;
-import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.core.PeerGroup;
+import org.bitcoinj.core.*;
 import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.net.discovery.DnsDiscovery;
 import org.bitcoinj.store.BlockStoreException;
 import org.bitcoinj.store.MemoryBlockStore;
+import org.bitcoinj.wallet.SendRequest;
 import org.bitcoinj.wallet.UnreadableWalletException;
 import org.bitcoinj.wallet.Wallet;
 import org.knowm.xchange.currency.CurrencyPair;
@@ -26,6 +24,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -53,42 +52,71 @@ public class BitcoinBalanceCheck {
     private KrakenExchange krakenExchange;
 
 
-    @Scheduled(initialDelay = 2000, fixedRate = 10000)
+    @Scheduled(initialDelay = 2000, fixedDelay = 60000)
     public void checkBalance() throws UnreadableWalletException {
-        WalletAppKit kit = null;
+        try {
+            WalletAppKit kit = null;
 
-        List<PaymentMethodBitcoinEntity> accounts = paymentMethodDao.findByType_Id("BTC").stream().map(m -> (PaymentMethodBitcoinEntity) m).collect(Collectors.toList());
+            List<PaymentMethodBitcoinEntity> accounts = paymentMethodDao.findByType_Id("BTC").stream().map(m -> (PaymentMethodBitcoinEntity) m).collect(Collectors.toList());
 
-        if (accounts.size() > 0) {
-            kit = new WalletAppKit(networkParameters, new File("."), "malevich-btc");
-            kit.startAsync();
-            kit.awaitRunning();
-        }
+            if (accounts.size() > 0) {
+                kit = new WalletAppKit(networkParameters, new File("."), "malevich-btc");
+                kit.startAsync();
+                kit.awaitRunning();
+            }
 
-        for (PaymentMethodBitcoinEntity account : accounts) {
-            Wallet wallet = Wallet.loadFromFileStream(new ByteArrayInputStream(account.getWallet()));
-            wallet.addWatchedAddress(new Address(networkParameters, account.getBtcAddress()));
-            BlockChain chain;
-            try {
-                chain = new BlockChain(networkParameters, wallet, memoryBlockStore);
+            for (PaymentMethodBitcoinEntity account : accounts) {
+                Wallet wallet = Wallet.loadFromFileStream(new ByteArrayInputStream(account.getWallet()));
+                wallet.addWatchedAddress(new Address(networkParameters, account.getBtcAddress()));
+                BlockChain chain;
+                try {
+                    chain = new BlockChain(networkParameters, wallet, memoryBlockStore);
 
-                PeerGroup peerGroup = new PeerGroup(networkParameters, chain);
-                peerGroup.addPeerDiscovery(new DnsDiscovery(networkParameters));
-                peerGroup.addWallet(wallet);
-                peerGroup.setFastCatchupTimeSecs(Constants.APPLICATION_RUN_TIME);
-                peerGroup.start();
-                peerGroup.downloadBlockChain();
-                System.out.println("!!!! Fucking balance = " + wallet.getBalance());
-                peerGroup.stop();
-            } catch (BlockStoreException e) {
-                e.printStackTrace();
+                    PeerGroup peerGroup = new PeerGroup(networkParameters, chain);
+                    peerGroup.addPeerDiscovery(new DnsDiscovery(networkParameters));
+                    peerGroup.addWallet(wallet);
+                    peerGroup.setFastCatchupTimeSecs(Constants.APPLICATION_RUN_TIME / 1000 - 1440);
+                    peerGroup.start();
+                    peerGroup.downloadBlockChain();
+
+
+                    System.out.println("!!!! Fucking balance = " + wallet.getBalance());
+
+                    if (wallet.getBalance().getValue() > 0) {
+                        send(wallet, "blablab", wallet.getBalance().getValue());
+                    }
+
+
+                    ByteArrayOutputStream walletDump = new ByteArrayOutputStream();
+                    try {
+                        wallet.saveToFileStream(walletDump);
+                        account.setWallet(walletDump.toByteArray());
+                        paymentMethodDao.save(account);
+                    } catch (IOException e) {
+                        new RuntimeException("Unable to save wallet seed");
+                    }
+                    peerGroup.stop();
+                } catch (BlockStoreException e) {
+                    e.printStackTrace();
+                }
             }
             if (accounts.size() > 0) {
                 kit.stopAsync();
                 kit.awaitTerminated();
             }
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
+    }
 
+    public Transaction send(Wallet wallet,
+                            String destinationAddress,
+                            long satoshis) throws Exception {
+        Address dest = Address.fromBase58(networkParameters, destinationAddress);
+        SendRequest request = SendRequest.to(dest, Coin.valueOf(satoshis));
+        Wallet.SendResult result = wallet.sendCoins(request);
+        Transaction endTransaction = result.broadcastComplete.get();
+        return endTransaction;
     }
 
     private void placeOrder(Wallet wallet, PaymentMethodEntity paymentMethodEntity) {

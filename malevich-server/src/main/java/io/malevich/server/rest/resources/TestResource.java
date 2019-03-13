ae3eee63d24a756dc4ef6d673ecbc11c8869b34e
thefattony <anton.alexeyev@outlook.com>
2019-03-13 15:16:49 +0300

    commision firts deal

--- malevich-server/src/main/java/io/malevich/server/rest/resources/TestResource.java
+++ malevich-server/src/main/java/io/malevich/server/rest/resources/TestResource.java
@@ -1,9 +1,12 @@
 package io.malevich.server.rest.resources;
 
 
+import io.malevich.server.twilio.SmsService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
@@ -11,5 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/test")
 public class TestResource {
 
+//    @Autowired
+//    private SmsService smsService;
+//
+//    @RequestMapping(value = "/smsService", method = RequestMethod.GET)
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public ResponseEntity<Void> smsService() {
+//        smsService.sendSms("+79258815879", "Привет от malevich.io");
+//
+//        return ResponseEntity.ok().build();
+//    }
 
 }

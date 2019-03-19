d983c6fd870c0d536f53965ec4d0c29972a50771
thefattony <anton.alexeyev@outlook.com>
2019-03-19 12:00:26 +0300

    kraken config changed

--- malevich-server/src/main/java/io/malevich/server/config/ExchangesConfig.java
+++ malevich-server/src/main/java/io/malevich/server/config/ExchangesConfig.java
@@ -3,18 +3,28 @@ package io.malevich.server.config;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.kraken.KrakenExchange;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ExchangesConfig {
 
+    @Value("${exchanges.kraken.username}")
+    private String userName;
+
+    @Value("${exchanges.kraken.apikey}")
+    private String apiKey;
+
+    @Value("${exchanges.kraken.secretkey}")
+    private String secretKey;
+
     @Bean
     public KrakenExchange krakenExchange() {
         ExchangeSpecification spec = new ExchangeSpecification(KrakenExchange.class);
-        spec.setApiKey("ub2kSjRtW87oViLK1iZgpDznDeQjkm9j9iMFHuDvlAehlKERlrd1HdM4");
-        spec.setSecretKey("CwU6v/UbnxNTgjAJ1RB3GZg1rcXC8lKNFp949YV+/gAICTvDvDXm0dsWEIzI2VcFkz6yW7/YkT+yAeJhl9ACGw==");
-        spec.setUserName("Vivin24");
+        spec.setApiKey(apiKey);
+        spec.setSecretKey(secretKey);
+        spec.setUserName(userName);
 
         KrakenExchange krakenExchange = (KrakenExchange) ExchangeFactory.INSTANCE.createExchange(spec);
         return krakenExchange;

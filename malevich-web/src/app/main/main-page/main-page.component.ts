8286c031bfd8ba600f98f8f395b5f0a83c493468
thefattony <anton.alexeyev@outlook.com>
2019-03-22 14:38:23 +0300

    main page from landing

--- malevich-web/src/app/main/main-page/main-page.component.ts
+++ malevich-web/src/app/main/main-page/main-page.component.ts
@@ -15,14 +15,6 @@ export class MainPageComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    var countdowns = $['HSCore'].components['HSCountdown'].init('.js-countdown', {
-      yearsElSelector: '.js-cd-years',
-      monthElSelector: '.js-cd-month',
-      daysElSelector: '.js-cd-days',
-      hoursElSelector: '.js-cd-hours',
-      minutesElSelector: '.js-cd-minutes',
-      secondsElSelector: '.js-cd-seconds'
-    });
   }
 
 }

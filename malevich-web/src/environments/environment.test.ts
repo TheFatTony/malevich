9cca1db8a091e3a73b0cc3504ad36dfcab5edd89
thefattony <anton.alexeyev@outlook.com>
2019-04-01 11:05:33 +0300

    s3 buckets

--- malevich-web/src/environments/environment.test.ts
+++ malevich-web/src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://ec2-54-172-12-7.compute-1.amazonaws.com:8080/malevich-srv/',
+  baseUrl: 'malevich-srv/',
   stripeKey: 'pk_test_M7nK7roFFAJEZPiw9jRKV60w'
 };
